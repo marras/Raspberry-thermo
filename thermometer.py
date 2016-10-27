fcfcfa319b64ff978d996a3100f70bdfa103ef09
Marras <marek.waligorski@gmail.com>
2016-10-27 21:45:41 +0200

    Send an md5 hash along with the payload

--- thermometer.py
+++ thermometer.py
@@ -5,15 +5,18 @@ import sensor
 import time
 import requests
 import json
+import hashlib
 import RPi.GPIO as GPIO
 
 def handleSuccessfulRead (temps):
     display.println(1, "Hey Babe! %s" % temps[0])
     display.println(2, "%s %s" % (temps[1], temps[2]))
     print(temps[0], temps[1], temps[2])
-    data = {'values': {'Temp1': temps[0], 'Temp2': temps[1], 'Temp3': temps[2]} }
+    data = {'Temp1': temps[0], 'Temp2': temps[1], 'Temp3': temps[2]}
+    doc_to_sign = json.dumps(data)
+    signature = hashlib.md5(doc_to_sign).hexdigest()
     headers = {'Content-Type': 'application/json'}
-    r = requests.post('http://lab-monitor.herokuapp.com/data', data = json.dumps(data), headers=headers)
+    r = requests.post('http://lab-monitor.herokuapp.com/data', data = json.dumps({"values": data, "signature": signature}), headers=headers)
     time.sleep(1)
 
 def main():
