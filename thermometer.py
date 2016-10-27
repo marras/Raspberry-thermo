65f22ae1e684b54342a3d76c843bcd3caa965de4
Marras <marek.waligorski@gmail.com>
2016-10-27 19:12:22 +0000

    Fix headers

--- thermometer.py
+++ thermometer.py
@@ -11,9 +11,9 @@ def handleSuccessfulRead (temps):
     display.println(1, "Hey Babe! %s" % temps[0])
     display.println(2, "%s %s" % (temps[1], temps[2]))
     print(temps[0], temps[1], temps[2])
-    data = json.dumps({'values': {'Temp1': temps[0], 'Temp2': temps[1], 'Temp3': temps[2]} })
-    headers = {'Content-Type: application/json; charset=utf-8'}
-    r = requests.post('http://lab-monitor.herokuapp.com/data', data = data, headers = headers)
+    data = {'values': {'Temp1': temps[0], 'Temp2': temps[1], 'Temp3': temps[2]} }
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post('http://lab-monitor.herokuapp.com/data', data = json.dumps(data), headers=headers)
     time.sleep(1)
 
 def main():
