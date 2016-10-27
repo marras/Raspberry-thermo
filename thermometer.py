08fb2596fe2f083fd470b343ad67072ac945641c
Marras <marek.waligorski@gmail.com>
2016-10-28 00:07:19 +0200

    Read signing code and add to the request

--- thermometer.py
+++ thermometer.py
@@ -8,15 +8,19 @@ import json
 import hashlib
 import RPi.GPIO as GPIO
 
+signing_code = ''
+
 def handleSuccessfulRead (temps):
     display.println(1, "Hey Babe! %s" % temps[0])
     display.println(2, "%s %s" % (temps[1], temps[2]))
     print(temps[0], temps[1], temps[2])
     data = {'Temp1': temps[0], 'Temp2': temps[1], 'Temp3': temps[2]}
-    doc_to_sign = json.dumps(data)
+    doc_to_sign = json.dumps(data, separators=(',',':')) + signing_code
     signature = hashlib.md5(doc_to_sign).hexdigest()
     headers = {'Content-Type': 'application/json'}
     r = requests.post('http://lab-monitor.herokuapp.com/data', data = json.dumps({"values": data, "signature": signature}), headers=headers)
+    if r.status_code != 200:
+        print "Failed to send data to server! %s" % r.text
     time.sleep(1)
 
 def main():
@@ -33,6 +37,9 @@ def main():
 
 if __name__ == '__main__':
     try:
+	f = open('signing_code')
+	signing_code = f.read().rstrip()
+	f.close()
         main()
     except KeyboardInterrupt:
         GPIO.cleanup()
