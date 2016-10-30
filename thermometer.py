3cf2c691e966b87e06d52cac8d0bc26c9d7fb7e5
Marras <marek.waligorski@gmail.com>
2016-10-30 11:46:24 +0100

    Nicer logging + handle network errors

--- thermometer.py
+++ thermometer.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import display
 import sensor
-import time
+from time import gmtime, strftime, sleep
 import requests
 import json
 import hashlib
@@ -11,17 +11,24 @@ import RPi.GPIO as GPIO
 signing_code = ''
 sleep_time = 0
 
+def log(*args):
+    timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    print timestamp, args
+
 def handleSuccessfulRead (temps):
     display.println(1, "Hey Babe! %s" % temps[0])
     display.println(2, "%s %s" % (temps[1], temps[2]))
-    print(temps[0], temps[1], temps[2])
+    log(temps[0], temps[1], temps[2])
     data = {'Temp1': temps[0], 'Temp2': temps[1], 'Temp3': temps[2]}
     doc_to_sign = json.dumps(data, separators=(',',':')) + signing_code
     signature = hashlib.md5(doc_to_sign).hexdigest()
     headers = {'Content-Type': 'application/json'}
-    r = requests.post('http://lab-monitor.herokuapp.com/data', data = json.dumps({"values": data, "signature": signature}), headers=headers)
-    if r.status_code != 200:
-        print "Failed to send data to server! %s" % r.text
+    try:
+        r = requests.post('http://lab-monitor.herokuapp.com/data', data = json.dumps({"values": data, "signature": signature}), headers=headers)
+        if r.status_code != 200:
+            log("Failed to send data to server! %s" % r.text)
+    except ConnectionError as ex:
+        log("Cannot connect to server! Error: %s" % ex.strerror)
 
 def main():
     display.lcd_init()
@@ -31,11 +38,11 @@ def main():
         if len(temps) != 3:
             display.println(1, "SENSOR ERROR!")
             display.println(2, "Sensors avail: %d" % len(temps))
-            print "Error: only %d sensors available!" % len(temps)
+            log("Error: only %d sensors available!" % len(temps))
         else:
             handleSuccessfulRead(temps)
 
-	time.sleep(sleep_time)
+	sleep(sleep_time)
 
 if __name__ == '__main__':
     try:
